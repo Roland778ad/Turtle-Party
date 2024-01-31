02f1893fd126915a1f9f5bc91e12cdef2bcd6aae
Roland778ad <roland778ad@gmail.com>
2024-01-31 10:38:57 -0500

    Python for everyone chapter 2

---
+++ Python Training/P4E.py
@@ -0,0 +1,11 @@
+# Python for Everyone Project
+# by Roland Udvari
+# 1-30-2024
+
+# INPUT usage
+nam = input("Hi, what is your name?")
+print("Hello", nam)
+byear = input("What year were you born in?")
+age = 2024 - int(byear)
+print("You are", age, "years old this year.")
+
---
+++ Python Training/practice_canvas.py
@@ -0,0 +1,26 @@
+# Practice Canvas for individual code blocks
+# By Roland Udvari
+# Created 1-30-2024
+
+# Prime finder
+# print("Let's see if I can figure out if a number is a prime number")
+# prm = input("What's your number: ")
+# prm1 = int(prm)/2
+#
+# for i in range(2, prm1):
+#     if int(prm) % i == 0:
+#         print("This is not a prime number.")
+#     if int(prm) % i > 0:
+#         print("This is a prime number.")
+
+for i in range(5):
+    print(i)
+    if i == 2 :
+        print("Equal to 2")
+    else:
+        if i > 2 :
+            print("Bigger than 2")
+        else:
+            print("Smaller than 2")
+print()
+print("All done")
