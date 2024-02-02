a87b154635b0008ba0b2237888d31ee95ff14095
Roland778ad <roland778ad@gmail.com>
2024-02-02 14:14:20 -0500

    Chapter 8 List operations

--- Python Training/practice_canvas.py
+++ Python Training/practice_canvas.py
@@ -69,10 +69,87 @@
 # print("The sum of all the numbers is", sum + ".")
 # print("The average of all the numbers is", avg + ".")
 
-# SLICING STRINGS
-word = 'bananaxblimeynana'
-i = word.find('x')
-i2 = word.find("na", i+1)
-print('x is in',i, 'position')
-print('the next ''na'' after ''x'' is in', i2, 'position')
-print('the text between ''x'' and ''na'' is:', word[i+1:i2])
+# ch6 SLICE STRIP STRINGS
+# src = 'X-DSPAM-confidence: 0.0475 '
+# i = src.find(':')
+# print('position of :', i)
+# a = src[i+1:]
+# print('string slice of the source data', a)
+# b = a.strip()
+# print('stripped string', b)
+# c = float(b)
+# print("number with float test", c+42)
+# print()
+# piece = float(src[src.find(':')+1:].strip())
+# print('single line code with float test: ', piece+42)
+
+# ch7 EXTERNAL FILE OPERATIONS
+# while True :
+#     file = input('Enter file, ot type '"Quit"' to quit: ')
+#     if file != "Quit" :
+#         try :
+#             data = open(file)
+#             break
+#         except :
+#             print("Invalid file.")
+#             continue
+#     print('Done')
+#     quit()
+#
+# count = 0
+# for line in data:
+#     count = count+1
+# print('Line count of file:', count)
+#
+# data = open(file)
+# inp = data.read()
+# print("There are ", len(inp), 'characters in this file.')
+# print(inp[:21])
+#
+# data = open(file)
+# count = 0
+# for fromline in data :
+#     fromline = fromline.rstrip()
+#     if not fromline.startswith("From") :
+#         continue
+#     if not 'uct.ac.za' in fromline :
+#         continue
+#     count = count + 1
+#     print(count, fromline)
+# print('done')
+
+# CH8 LISTS
+# friends = ["Joe", "Glenn", "Sally"]
+# print(friends)
+# print(len(friends))
+# print(friends[1])
+# print()
+#
+# for friend in friends :
+#     print("Happy New Year,", friend + "!")
+#
+# for x in range(len(friends)-1) :
+#     print('Happy Birthday,', friends[1] + '!')
+
+
+listnum = [1, 56, 98, 0, 33, 687, 1598]
+print('Numbers so far:', listnum)
+while True :
+    inp = input('Add any numbers to this list (when finished, type "done"): ')
+    if inp == 'done' :
+        break
+    splitinp = inp.split(',')
+    for numb in splitinp :
+        listnum.append(int(numb))
+print(listnum)
+listnum.sort()
+print('All numbers in order:', listnum)
+print('The lowest number on your list is', min(listnum))
+print('The highest number on your list is', max(listnum))
+print('You have entered', len(listnum), 'number(s).')
+sumnum = str(sum(listnum))
+print('The sum of all numbers is', sumnum + '.')
+print('The average of all numbers is:', sum(listnum) / len (listnum))
+print('DONE')
+
+
