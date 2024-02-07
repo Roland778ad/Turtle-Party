9683200b347c874fc26f968cb0ff59fac184eb03
Roland778ad <roland778ad@gmail.com>
2024-02-07 11:06:00 -0500

    Chapter 12 Network

--- Python Training/practice_canvas.py
+++ Python Training/practice_canvas.py
@@ -231,28 +231,69 @@ hand = open('sample_text.txt')
 #             if '@' in email :
 #                 print(email)
 
-print ()
-import re
-
-print()
-hand = open('sample_text.txt')
-
-# for line in hand :
-#     line = line.rstrip()
-#     if re.search('^From:', line) :
-#         print(line)
-
+# print ()
+# import re
+#
+# print()
+# hand = open('sample_text.txt')
+#
+# # for line in hand :
+# #     line = line.rstrip()
+# #     if re.search('^From:', line) :
+# #         print(line)
+#
+# # for lines in hand :
+# #     if lines.startswith("From") :
+# #         line = lines.rstrip()
+# #         print(re.findall('^From (\S+@\S+)', line))
+# emails = list()
 # for lines in hand :
-#     if lines.startswith("From") :
-#         line = lines.rstrip()
-#         print(re.findall('^From (\S+@\S+)', line))
-emails = list()
-for lines in hand :
-    line = lines.strip()
-    y = re.findall('^From (.*@[^ ]*)', line)
+#     line = lines.strip()
+#     y = re.findall('^From (.*@[^ ]*)', line)
+#
+#     if len(y) != 1 :continue
+#     emails.append(y)
+# print(len(emails))
+# print(emails)
 
-    if len(y) != 1 :continue
-    emails.append(y)
-print(len(emails))
-print(emails)
+# CHAPTER 10 NETWORK
 
+# import socket
+#
+# mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# mysock.connect(('data.pr4e.org', 80))
+# cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+# mysock.send(cmd)
+#
+# while True:
+#     data = mysock.recv(512)
+#     if len(data) < 1:
+#         break
+#     print(data.decode(),end='')
+# mysock.close()
+
+# import urllib.request, urllib.parse, urllib.error
+# fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+# for line in fhand :
+#  print(line.decode().strip())
+
+# import urllib.request, urllib.parse, urllib.error
+# from bs4 import BeautifulSoup
+# import re
+# url = input('Enter web address: ')
+# html = urllib.request.urlopen(url).read().decode()
+#
+#soup = BeautifulSoup(html, 'html.parser')
+# tags = soup('a')
+# for tag in tags:
+#     print(tag.get('href', None))
+#
+# words = html.split()
+# #print(words)
+# for href in words:
+#     if href.startswith('href'):
+#        print(href[6:(-1)])
+#        hrefcln = re.findall('"(\\S*)"', href)
+#        print(hrefcln)
+
+#CHAPTER 13
