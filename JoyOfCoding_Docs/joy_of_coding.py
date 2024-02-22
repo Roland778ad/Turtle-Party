9ca1b3176a303b62ec879a58eca1cb2e5ea9672a
Roland778ad <roland778ad@gmail.com>
2024-02-22 12:40:12 -0500

    Py training files reorg

---
+++ JoyOfCoding_Docs/joy_of_coding.py
@@ -0,0 +1,54 @@
+#JOC PY BASICS
+
+# import math
+# from math import sqrt
+# print("Give me two numbers:")
+# num1 = input("First number: ")
+# num2 = input("Second number: ")
+# if int(num1) < 0:
+#     print("Negative numbers dont have square roots, silly!")
+# if int(num1) > 0:
+#     print("Here's the square root of", num1, "=", math.sqrt(int(num1)))
+# if int(num2) < 0:
+#     print("Negative numbers dont have square roots, silly!")
+# if int(num2) > 0:
+#     print("Here's the square root of", num2, "=", sqrt(int(num2)))
+#
+# print()
+# for hi in range(5):
+#     print(hi)
+#
+# print()
+# print("-" * 10)
+#
+# print()
+# for lo in range(7, -7, -3):
+#     print(lo)
+#
+# print()
+# print("-" * 10)
+#
+# print()
+# phrase = "Monty Python\n", "\nMonty Python\n"
+# for letter in phrase:
+#     letter.rstrip()
+#     print(letter, end="-")
+# print()
+#
+# print("END")
+# print()
+#
+# # FUNCTION
+# def add(x, y):
+#     print("I am going to add the numbers %d and %d." % (x, y))
+#     return x + y
+#
+# print("Functions")
+# print()
+# sum = add(int(num1), int(num2))
+# print("=", sum)
+
+# =========THE % CODE============
+num = 300
+num1 = 8
+print(num % num1)
